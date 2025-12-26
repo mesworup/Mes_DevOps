12213658d69eb6f615fa75bdfbbd83481a418373
sworup razz <razzsworup.com>
2025-12-26 22:01:16 +0545

    7th script added

---
+++ Bashscripts/7.Command_Substitution.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#command_substitution
+myfilecontent=`cat ./myfile.txt`
+echo $myfilecontent
+
+echo "Another approach"
+fileContents=$(cat ./myfile.txt)
+echo "The contents of file is \n $fileContents"
